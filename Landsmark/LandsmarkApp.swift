edd2a6f5f43ae115716fd808efa8dc67d0b16a7b
NewajShovo <leoshovo@gmail.com>
2025-05-19 19:50:36 +0600

    Initial Commit

---
+++ Landsmark/LandsmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandsmarkApp.swift
+//  Landsmark
+//
+//  Created by Shovo on 19/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LandsmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandsmarkTests/LandsmarkTests.swift
@@ -0,0 +1,16 @@
+//
+//  LandsmarkTests.swift
+//  LandsmarkTests
+//
+//  Created by Shovo on 19/5/25.
+//
+
+import Testing
+
+struct LandsmarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
