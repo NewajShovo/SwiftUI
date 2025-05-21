cc51908c1531f2b2a0c6e3ae6e902427fb92d13a
NewajShovo <leoshovo@gmail.com>
2025-05-21 19:58:22 +0600

    Set up navigation link with rows

---
+++ Landsmark/Views/LandmarkDetail.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarkDetail.swift
+//  Landsmark
+//
+//  Created by Shovo on 21/5/25.
+//
+
+import SwiftUI
+
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
